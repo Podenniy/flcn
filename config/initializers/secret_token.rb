7eb4c4d48df3154430e7705e57b827193d057094
Dmitry <budyachocheck@gmail.com>
2013-12-27 22:48:37 +0200

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FlcnMotrs::Application.config.secret_key_base = '68c3365fee9667251cd0b44608117eee7c4d6f728bf524e3dab405b50918edef115ef5a5a6b09adc7f722bd59d80bb720eccb72bce58d1670727c3fa06584c3c'
